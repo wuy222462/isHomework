554d618b0feee1c41585f1f59fbec9f4650ecf8f
Tom <wuy222462@gmail.com>
2022-09-19 16:21:17 +0800

    面向过程3 更改项目名字 并无实际内容添加

--- 面向过程2/Program.cs
+++ 面向过程2/Program.cs
@@ -14,17 +14,7 @@ class Class1
     static void Main()
     {
 
-        //声明变量sname（学生姓名），将你的名字（比如：阿泰）赋值给它；然后利用它输出入栈口号，比如：我是阿泰，学业不成，誓不出栈！
-
-
-        string name;
-
-        name = Console.ReadLine();
-
-        Console.WriteLine("我是" + name + "，学业不成，誓不出栈！");
-
-
-
+       
 
 
 

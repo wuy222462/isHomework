c71bdc319b221fe564ff2da78c91a82ee22f173a
Tom <wuy222462@gmail.com>
2022-09-19 17:31:57 +0800

    for循环，计算出1000 + 999 + 998 + 997 +……+100/*的值*/

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -15,16 +15,17 @@ class Class1
     {
         //分别利用while和for循环，计算出1000 + 999 + 998 + 997 +……+100/*的值*/
         int i, sum;
-        i = 100;
+
         sum = 0;
-        while (i <= 1000)
+        for (i = 100; i <= 1000; i++)
         {
             sum += i;
+
             Console.WriteLine(sum);
-            i++;
+
+
 
         }
-
 
     }
 

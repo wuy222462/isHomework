458709f1e3d916d8365fbef593a214dc2f065038
Tom <wuy222462@gmail.com>
2022-09-19 15:09:32 +0800

    判断是不是正数，正数的话输出输入的数字是正数 不是的话重新输入

--- 面向过程2/Program.cs
+++ 面向过程2/Program.cs
@@ -1,43 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project
-{
-    class Class1
+namespace Project;
 
+class Class1
+
+{
+
+    static void Main()
     {
 
-        static void Main()
+        //            声明一个变量number，给number赋整数值，写出以下表达式，判断number是不是：
+        //正数，示例：
+        //number > 0
+        //大于100的正数
+        //大于100小于1000的正数
+        //10000以内能被3整除的奇数
+
+
+        int number;
+        number = int.Parse(Console.ReadLine());
+        if (number > 0)
         {
 
-            //写一段代码，能根据半径获取圆的周长（= 2倍半径乘以圆周率）和面积（= 半径的平方乘以圆周率）
-            //声明一个变量radius，代表半径，给它赋一个任意值；
-            //使用alert(/*这里放表达式*/)输出周长;
-            //使用alert(/*这里放表达式*/)输出面积;
-            double pai, radius;
-            pai = 3.14F;
-            radius = double.Parse(Console.ReadLine());
-            Console.WriteLine("周长="+2*radius*pai);  
-            Console.WriteLine("面积="+radius*pai*radius);
+            Console.WriteLine(number + "是正数");
+
+        }
+        else {
+
+            Console.WriteLine("重新输入");
+        
+        }
+    
+    }
+
+}
+
+
+
 
             
 
-
-        }
-
-
-
-
-    }
+        
 
 
 
 
 
-}
+
+
+
+    
+
+
+
 
 
 

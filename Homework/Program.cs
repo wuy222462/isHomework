d3606551a80d3c252a6a3a92c4908d4c0cbe790a
Tom <wuy222462@gmail.com>
2022-09-19 17:54:47 +0800

    声明一个数组ids  for 输出每个元素的值

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -13,16 +13,26 @@ class Class1
 
     static void Main()
     {
-        //让电脑计算并输出：99 + 97 + 95 + 93 + ...+1的值
+        //        声明一个数组ids，存放若干整数，利用：
+        //while
+        //for循环 i--
+        //输出数组中每个元素的值
 
-        int i, sum;
-        sum = 0;
-        for (i = 1; i <= 99; i += 2)
+        //int[] ids;
+        //ids = new int[] { 21, 32, 421, 4124, 515 };
+
+        int[] ids = { 21, 32, 421, 4124, 515 };
+        int i;//不声明i for循环也不能直接用;
+
+        for (i = 0; i < ids.Length; i++)
         {
-            sum += i;
+            Console.WriteLine(ids[i]);
 
         }
-        Console.WriteLine(sum);//只输出一次和
+
+
+
+
 
     }
 

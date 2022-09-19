eef80c197e7db9cc178e2202df3ce8f043322ed5
Tom <wuy222462@gmail.com>
2022-09-19 16:18:57 +0800

    声明变量sname（学生姓名），将你的名字（比如：阿泰）赋值给它；然后利用它输出入栈口号，比如：我是阿泰，学业不成，誓不出栈！

--- 面向过程2/Program.cs
+++ 面向过程2/Program.cs
@@ -14,35 +14,14 @@ class Class1
     static void Main()
     {
 
-        //    想一想如下表达式的结果是什么，并说明原因：
-        //let a = 10;
-        //    console.log(a > 9 && (!(a < 11) || a > 10));
+        //声明变量sname（学生姓名），将你的名字（比如：阿泰）赋值给它；然后利用它输出入栈口号，比如：我是阿泰，学业不成，誓不出栈！
 
 
+        string name;
 
+        name = Console.ReadLine();
 
-        //回答：是false ，
-
-        //        a>9是符合的结果为true，
-
-        //        &&后面的条件是（）
-
-        //        !(a < 11) 这代表a>11  返回false
-        //        或者a > 10 返回 false
-
-        //        ture&&false 
-
-        //        返回false
-
-        //当a为何值时，结果为true？
-
-        //    let result = (a + 3 > 12) && a < 3.14 * 4 && a != 11;
-        //大于9且 小于12.56 并且a不等于11
-        //   所以a为10||12
-
-             
-
-
+        Console.WriteLine("我是" + name + "，学业不成，誓不出栈！");
 
 
 

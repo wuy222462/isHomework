4cc15ebc0ea811a12c719da65da95a85add7ada6
Tom <wuy222462@gmail.com>
2022-09-19 16:59:11 +0800

    for 循环输出12345

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -13,6 +13,14 @@ class Class1
 
     static void Main()
     {
+        //分别用for循环和while循环直接输出：1,2,3,4,5 和 1,3,5,7,9
+        int i;
+        for (i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+
 
 
 
